9f541a14689b0fd5c011098c5635d8e216740ec0
kyomi <atrpwq@proton.me>
2024-09-28 10:14:35 +0300

    Update vid.py

--- vid.py
+++ vid.py
@@ -1,5 +1,2 @@
-from pytube import YouTube
-url = input("url: ")
-yt = YouTube(url)
-print(f"title {yt.title}")
-yt.streams.get_highest_resolution().download("vids")
+from pytubefix import YouTube
+YouTube(input("url: ")).streams.get_highest_resolution().download("vids")
